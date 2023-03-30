7c42697ee14fb37f7d303a37aa03fc303f308134
VGorash <gorash590@gmail.com>
2023-03-30 13:39:50 +0300

    progress bar

--- src/data_loading/fill_database.py
+++ src/data_loading/fill_database.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 import psycopg2
+from progress.bar import Bar
 
 
 def connect_database():
@@ -18,12 +19,18 @@ def process_csv(filename):
     cursor = connection.cursor()
     cursor.execute("DROP TABLE IF EXISTS dataset")
     cursor.execute("CREATE TABLE dataset(id int primary key, resume text, category text)")
+    print("Uploading data to database")
+    bar = Bar("Upload progress", max=len(df))
     for index, row in df.iterrows():
         cursor.execute("INSERT INTO dataset(id, resume, category) values (%s, %s, %s)",
                        (index, row["Resume"], row["Category"]))
+        bar.next()
+    bar.finish()
     cursor.close()
+    print("Committing changes")
     connection.commit()
     connection.close()
+    print("Data uploaded")
 
 
 if __name__ == "__main__":
